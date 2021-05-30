5265baf725205441995337c5473ac7a7fd40ce38
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2021-05-29 23:57:19 -0300

    formatar a resposta como um objeto arredondar a precisão da tradução

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ app = Flask(__name__)
 @app.route("/describe")
 def describe_api():
     url = request.args.get('url')
-    return jsonify(describe_image(url))
+    description = describe_image(url)
+    api_response = {}
+    api_response["description"] = description[0]
+    api_response["confidence"] = round(description[1])
+    return jsonify(api_response)
